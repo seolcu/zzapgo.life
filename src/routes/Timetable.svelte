ab829106e5810d87c7ee4c51dd685d74399b2ba0
seolcu <gyuwon112@gmail.com>
2021-09-05 00:05:38 +0900

    README

--- src/routes/Timetable.svelte
+++ src/routes/Timetable.svelte
@@ -4,7 +4,13 @@
 </script>
 
 <Header content="시간표" />
+<div class="timetableContainer" style="display: none;" />
 <Footer currentURL="/timetable" />
 
 <style>
+  .timetableContainer {
+    display: grid;
+    grid-template-columns: repeat(6, 1fr);
+    grid-template-rows: repeat(8, 1fr);
+  }
 </style>
