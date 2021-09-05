1927756b995e403c890e230cb6b228c877084759
seolcu <gyuwon112@gmail.com>
2021-09-05 20:29:50 +0900

    지금의사감 추가 but 시간 props 리팩터링 필요

--- src/components/DormManager.svelte
+++ src/components/DormManager.svelte
@@ -1,28 +1,65 @@
 <script>
   const dormInfo = {
+    // 오후 사감이 기준 !!!!
     "2021/9": {
       odd: {
         male: "이만수",
-        female: "",
+        female: "이순덕",
       },
       even: {
         male: "정진기",
-        female: "",
+        female: "장정숙",
       },
     },
   };
   const year = new Date().getFullYear();
-  const month = new Date().getMonth();
+  const month = new Date().getMonth() + 1; //getMonth는 0부터 11
   const date = new Date().getDate();
+  const hours = new Date().getHours();
+  const yearPlusMonth = year + "/" + month;
+  const getCurrentDormManager = (gender) => {
+    if (date % 2 == 1) {
+      // 홀수일
+      if (hours <= 12) {
+        // 오전: 반대로
+        return dormInfo[yearPlusMonth].even[gender];
+      } else {
+        // 오후
+        return dormInfo[yearPlusMonth].odd[gender];
+      }
+    } else {
+      // 짝수일
+      if (hours <= 12) {
+        //오전: 반대로
+        return dormInfo[yearPlusMonth].odd[gender];
+      } else {
+        // 오후
+        return dormInfo[yearPlusMonth].even[gender];
+      }
+    }
+  };
 </script>
 
 <div class="dormManagerWrapper">
-  <p>
-    남 - {date % 2 == 1} 사감
-  </p>
-  <p>
-    여 - {date % 2 == 1
-      ? dormInfo.yearPlusMonth.odd.female
-      : dormInfo.yearPlusMonth.even.female} 사감
-  </p>
+  <h2>지금의 사감</h2>
+  <h3>
+    {getCurrentDormManager("female")} 사감, {getCurrentDormManager("male")} 사감
+  </h3>
 </div>
+
+<style>
+  .dormManagerWrapper {
+    margin: 10px;
+    background: #333333;
+    color: #eeeeee;
+    padding: 10px;
+    border-radius: 20px;
+  }
+  .dormManagerWrapper h2 {
+    margin: 10px;
+  }
+  .dormManagerWrapper h3 {
+    text-align: center;
+    margin: 10px;
+  }
+</style>
