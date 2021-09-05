e92806c1bdbae4c2c16e3d045242a6074c5fbfe0
seolcu <gyuwon112@gmail.com>
2021-09-05 13:38:54 +0900

    시간표 컴포넌트 생성 but 왜인지 잘 안돼서 display none

--- src/routes/Timetable.svelte
+++ src/routes/Timetable.svelte
@@ -1,17 +1,13 @@
 <script>
   import Footer from "../components/Footer.svelte";
   import Header from "../components/Header.svelte";
+  import TimetableComponent from "../components/TimetableComponent.svelte";
 </script>
 
 <Header content="시간표" />
 
-<div class="timetableContainer" style="display: none;" />
+<TimetableComponent />
 <Footer currentURL="/timetable" />
 
 <style>
-  .timetableContainer {
-    display: grid;
-    grid-template-columns: repeat(6, 1fr);
-    grid-template-rows: repeat(8, 1fr);
-  }
 </style>
