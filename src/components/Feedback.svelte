e9ffc27c14f5bd860b07fa177fcfb647dd951346
seolcu <gyuwon112@gmail.com>
2021-09-06 09:34:58 +0900

    Feedback 컴포넌트 추가

---
+++ src/components/Feedback.svelte
@@ -0,0 +1,20 @@
+<div class="feedbackWrapper">
+  <h2>피드백</h2>
+  <h3>DM: @gyuwon112<br />Email: gyuwon112@gmail.com</h3>
+</div>
+
+<style>
+  .feedbackWrapper {
+    margin: 10px;
+    padding: 10px;
+    border-radius: 20px;
+    background: #333333;
+    color: #eeeeee;
+  }
+  h2 {
+    margin: 10px;
+  }
+  h3 {
+    margin: 10px;
+  }
+</style>
--- src/routes/About.svelte
+++ src/routes/About.svelte
@@ -1,5 +1,6 @@
 <script>
   import Developing from "../components/Developing.svelte";
+  import Feedback from "../components/Feedback.svelte";
 
   import Footer from "../components/Footer.svelte";
   import Header from "../components/Header.svelte";
@@ -7,6 +8,7 @@
 
 <Header content="더보기" />
 <Developing content="테마" />
+<Feedback />
 <Footer currentURL="/about" />
 
 <style>
