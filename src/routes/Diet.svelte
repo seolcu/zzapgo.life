19669427d895efc125fe249b91374680f9258c16
seolcu <gyuwon112@gmail.com>
2021-09-06 09:29:56 +0900

    Diet, About에 Developing 추가

--- src/routes/Diet.svelte
+++ src/routes/Diet.svelte
@@ -1,4 +1,5 @@
 <script>
+  import Developing from "../components/Developing.svelte";
   import Footer from "../components/Footer.svelte";
   import Header from "../components/Header.svelte";
   import OneMeal from "../components/OneMeal.svelte";
@@ -12,6 +13,7 @@
 <!-- getMonth는 0~11 이라 1 추가 -->
 <Header content="{new Date().getMonth() + 1}월 {new Date().getDate()}일" />
 
+<Developing content="날짜 선택" />
 {#each [0, 1, 2] as mealType}
   <OneMeal {mealType} {year} {month} {date} />
 {/each}
