a6bee4ead075a38f1c990f49ad4e5852387c57a3
seolcu <seolcu0112@proton.me>
2021-10-01 12:00:00 +0900

    router

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,18 @@
 import "./App.css";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
 import Main from "./routes/Main";
 
 const App = () => {
   return (
-    <>
-      <Main />
-    </>
+    <div className="App">
+      <BrowserRouter>
+        <Switch>
+          <Route path="/">
+            <Main />
+          </Route>
+        </Switch>
+      </BrowserRouter>
+    </div>
   );
 };
 
--- src/routes/Main.tsx
+++ src/routes/Main.tsx
@@ -2,9 +2,9 @@ import Home from "../pages/Home";
 
 const Main = () => {
   return (
-    <>
+    <div className="Main">
       <Home />
-    </>
+    </div>
   );
 };
 
