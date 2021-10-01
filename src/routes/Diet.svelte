016e3daada45f74fb8f73dc56dddbb33945ccfd7
seolcu <seolcu0112@proton.me>
2021-10-01 12:00:00 +0900

    사감 컴포넌트 추가 but 보류

--- src/routes/Diet.svelte
+++ src/routes/Diet.svelte
@@ -4,8 +4,8 @@
   import OneMeal from "../components/OneMeal.svelte";
 
   const year = new Date().getFullYear();
-  const month = ("0" + (new Date().getMonth() + 1)).slice(-2);
-  const date = ("0" + new Date().getDate()).slice(-2);
+  const month = ("0" + (new Date().getMonth() + 1)).slice(-2); // 두자리
+  const date = ("0" + new Date().getDate()).slice(-2); // 두자리
 </script>
 
 <Header content="{month}월 {date}일" />
@@ -13,7 +13,6 @@
 {#each [0, 1, 2] as mealType}
   <OneMeal {mealType} {year} {month} {date} />
 {/each}
-
 <Footer currentURL="/diet" />
 
 <style>
