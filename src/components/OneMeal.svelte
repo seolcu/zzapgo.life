64006eaf56da9b1aba8aa0d91abd443bc849228d
seolcu <seolcu0112@proton.me>
2021-10-01 12:00:00 +0900

    메뉴 알러지성분표기 제거

---
+++ src/components/OneMeal.svelte
@@ -0,0 +1,39 @@
+<script>
+  import axios from "axios";
+
+  export let year;
+  export let month;
+  export let date;
+  export let mealType; //조식 0 중식 1 석식 2
+
+  $: mealData = axios
+    .get(
+      `https://open.neis.go.kr/hub/mealServiceDietInfo?KEY=015f0705bbe0482589da35f787d46817&Type=json&pIndex=1&pSize=3&ATPT_OFCDC_SC_CODE=Q10&SD_SCHUL_CODE=8490078&MLSV_YMD=${year}${month}${date}`
+    )
+    .then((response) => response.data.mealServiceDietInfo[1].row);
+
+  const parseDataStringIntoList = (dataString) => {
+    return dataString.replace(/\./gi, "").replace(/[0-9]/g, "").split("<br/>"); //removing tags
+  };
+</script>
+
+<div>
+  <h2>
+    {mealType == 0
+      ? "아침"
+      : mealType == 1
+      ? "점심"
+      : mealType == 2
+      ? "저녁"
+      : "???"} 급식
+  </h2>
+  {#await mealData}
+    로딩중...
+  {:then mealData}
+    <ul>
+      {#each parseDataStringIntoList(mealData[mealType].DDISH_NM) as oneMenu}
+        <li>{oneMenu}</li>
+      {/each}
+    </ul>
+  {/await}
+</div>
--- src/routes/Diet.svelte
+++ src/routes/Diet.svelte
@@ -1,30 +1,17 @@
 <script>
-  import axios from "axios";
   import Footer from "../components/Footer.svelte";
+  import OneMeal from "../components/OneMeal.svelte";
 
   const year = new Date().getFullYear();
   const month = ("0" + (new Date().getMonth() + 1)).slice(-2);
   const date = ("0" + new Date().getDate()).slice(-2);
-
-  $: mealData = axios
-    .get(
-      `https://open.neis.go.kr/hub/mealServiceDietInfo?KEY=015f0705bbe0482589da35f787d46817&Type=json&pIndex=1&pSize=3&ATPT_OFCDC_SC_CODE=Q10&SD_SCHUL_CODE=8490078&MLSV_YMD=${year}${month}${date}`
-    )
-    .then((response) => response.data.mealServiceDietInfo[1].row);
 </script>
 
 <h1>{month}월 {date}일</h1>
 
-{#await mealData}
-  <p>로딩중...</p>
-{:then mealData}
-  <h2>아침 급식</h2>
-  {@html mealData[0].DDISH_NM}
-  <h2>점심 급식</h2>
-  {@html mealData[1].DDISH_NM}
-  <h2>저녁 급식</h2>
-  {@html mealData[2].DDISH_NM}
-{/await}
+{#each [0, 1, 2] as mealType}
+  <OneMeal {mealType} {year} {month} {date} />
+{/each}
 
 <Footer currentURL="/diet" />
 
