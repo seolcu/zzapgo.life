8fcccc55e99d1ea866177a03469fc215fd412453
seolcu <seolcu0112@proton.me>
2021-10-01 12:00:00 +0900

    big fixes with vite-pwa

--- vite.config.ts
+++ vite.config.ts
@@ -8,9 +8,40 @@ export default defineConfig({
     react(),
     VitePWA({
       registerType: "autoUpdate",
-      injectRegister: "script",
+      injectRegister: "auto",
       devOptions: {
         enabled: true,
+      },
+      includeAssets: ["favicon.ico", "apple-touch-icon.png", "mask-icon.svg"],
+      manifest: {
+        name: "짭고라이프",
+        short_name: "짭고라이프",
+        start_url: "/",
+        display: "standalone",
+        background_color: "#F6E05E",
+        dir: "ltr",
+        lang: "ko",
+        orientation: "any",
+        scope: "/",
+        theme_color: "#F6E05E",
+        icons: [
+          {
+            src: "pwa-192x192.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+          {
+            src: "pwa-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+          },
+          {
+            src: "pwa-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+            purpose: "any maskable",
+          },
+        ],
       },
     }),
   ],
