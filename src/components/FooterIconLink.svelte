9bd59e24050667c66d7e0421a63360b34fd2706f
seolcu <seolcu0112@proton.me>
2021-10-01 12:00:00 +0900

    footer 아이콘 바뀌게

---
+++ src/components/FooterIconLink.svelte
@@ -0,0 +1,15 @@
+<script>
+  import { link } from "svelte-spa-router";
+  export let selected;
+  export let name;
+  export let url;
+  export let iconName;
+</script>
+
+<a href={url} use:link>
+  {#if selected == name}
+    <img src="./images/{iconName}.svg" alt={name} />
+  {:else}
+    <img src="./images/{iconName}_gray.svg" alt={name} />
+  {/if}
+</a>
