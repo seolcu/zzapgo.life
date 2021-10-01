7e61a688d598584ea7029bb148e064abb3db0c50
seolcu <seolcu0112@proton.me>
2021-10-01 12:00:00 +0900

    메인페이지 급식 추가

--- src/routes/Home.svelte
+++ src/routes/Home.svelte
@@ -1,8 +1,20 @@
 <script>
   import Footer from "../components/Footer.svelte";
+  import OneMeal from "../components/OneMeal.svelte";
+  const year = new Date().getFullYear();
+  const month = ("0" + (new Date().getMonth() + 1)).slice(-2);
+  const date = ("0" + new Date().getDate()).slice(-2);
+  const hours = new Date().getHours();
 </script>
 
 <h1>홈</h1>
+<OneMeal
+  {year}
+  {month}
+  {date}
+  mealType={hours <= 8 ? 0 : hours <= 14 ? 1 : hours <= 19 ? 2 : 2}
+/>
+<!-- mealType: 0 조식 1 중식 2 석식 -->
 <Footer currentURL="/" />
 
 <style>
