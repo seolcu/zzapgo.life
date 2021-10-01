b258f617510e8640746e93352ae06e96c41753cd
seolcu <seolcu0112@proton.me>
2021-10-01 12:00:00 +0900

    글씨 크기 조정

--- components/HeaderComponent.js
+++ components/HeaderComponent.js
@@ -2,8 +2,8 @@ import styles from "./HeaderComponent.module.scss";
 
 const HeaderComponent = ({ title }) => {
   return (
-    <header className={styles.header}>
-      <h3>{title}</h3>
+    <header>
+      <h2 className={styles.header}>{title}</h2>
     </header>
   );
 };
