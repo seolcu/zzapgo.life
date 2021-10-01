51d5e88ae0d4d2227f489340dda599c25d503396
seolcu <seolcu0112@proton.me>
2021-10-01 12:00:00 +0900

    급식 표시

--- src/components/Footer.svelte
+++ src/components/Footer.svelte
@@ -1,44 +1,12 @@
 <script>
   import { link } from "svelte-spa-router";
-  import BookmarkIcon from "../icons/BookmarkIcon.svelte";
-  import CalendarIcon from "../icons/CalendarIcon.svelte";
-  import HomeIcon from "../icons/HomeIcon.svelte";
-  import MenuIcon from "../icons/MenuIcon.svelte";
-  import TimetableIcon from "../icons/TimetableIcon.svelte";
-
-  const linkInfo = [
-    {
-      name: "home",
-      url: "/",
-      icon: HomeIcon,
-    },
-    {
-      name: "diet",
-      url: "/diet",
-      icon: CalendarIcon,
-    },
-    {
-      name: "shortcut",
-      url: "/shortcut",
-      icon: BookmarkIcon,
-    },
-    {
-      name: "timetable",
-      url: "/timetable",
-      icon: TimetableIcon,
-    },
-    {
-      name: "about",
-      url: "/about",
-      icon: MenuIcon,
-    },
-  ];
+  import pageInfo from "../pageInfo";
 
   let selected = "home";
 </script>
 
 <footer>
-  {#each linkInfo as oneLink}
+  {#each pageInfo as oneLink}
     <a href={oneLink.url} use:link on:click={() => (selected = oneLink.name)}>
       <svelte:component
         this={oneLink.icon}
