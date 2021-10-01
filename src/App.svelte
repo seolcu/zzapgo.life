b2994792d1947c02bac02fc3de4b3c563112137c
seolcu <seolcu0112@proton.me>
2021-10-01 12:00:00 +0900

    routes 틀 잡기

---
+++ src/App.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+  import Router from "svelte-spa-router";
+  import Home from "./routes/Home.svelte";
+  import About from "./routes/About.svelte";
+  import Calendar from "./routes/Calendar.svelte";
+  import Shortcut from "./routes/Shortcut.svelte";
+  import Timetable from "./routes/Timetable.svelte";
+
+  const routes = {
+    "/": Home,
+    "/calendar": Calendar,
+    "/shortcut": Shortcut,
+    "/timetable": Timetable,
+    "/about": About,
+  };
+</script>
+
+<Router {routes} />
