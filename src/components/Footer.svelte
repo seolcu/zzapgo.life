2f4d97a3749541d04d3f2d11dc2e6a20922de7e4
seolcu <seolcu0112@proton.me>
2021-10-01 12:00:00 +0900

    아이콘 컴포넌트화, 빠릿빠릿하게 만듬🌟

--- src/components/Footer.svelte
+++ src/components/Footer.svelte
@@ -1,45 +1,50 @@
 <script>
-  import FooterIconLink from "./FooterIconLink.svelte";
-  let selected = "home";
+  import { link } from "svelte-spa-router";
+  import BookmarkIcon from "../icons/BookmarkIcon.svelte";
+  import CalendarIcon from "../icons/CalendarIcon.svelte";
+  import HomeIcon from "../icons/HomeIcon.svelte";
+  import MenuIcon from "../icons/MenuIcon.svelte";
+  import TimetableIcon from "../icons/TimetableIcon.svelte";
+
   const linkInfo = [
     {
       name: "home",
       url: "/",
-      icon: "home",
+      icon: HomeIcon,
     },
     {
       name: "diet",
       url: "/diet",
-      icon: "calendar",
+      icon: CalendarIcon,
     },
     {
       name: "shortcut",
       url: "/shortcut",
-      icon: "bookmark",
+      icon: BookmarkIcon,
     },
     {
       name: "timetable",
       url: "/timetable",
-      icon: "timetable",
+      icon: TimetableIcon,
     },
     {
       name: "about",
       url: "/about",
-      icon: "menu",
+      icon: MenuIcon,
     },
   ];
+
+  let selected = "home";
 </script>
 
 <footer>
   {#each linkInfo as oneLink}
-    <div on:click={() => (selected = oneLink.name)}>
-      <FooterIconLink
-        name={oneLink.name}
-        url={oneLink.url}
-        iconName={oneLink.icon}
-        {selected}
+    <a href={oneLink.url} use:link on:click={() => (selected = oneLink.name)}>
+      <svelte:component
+        this={oneLink.icon}
+        color={selected == oneLink.name ? "#FFFFFF" : "#888888"}
       />
-    </div>
+    </a>
   {/each}
 </footer>
 
---
+++ src/linkInfo.js
@@ -0,0 +1,29 @@
+const linkInfo = [
+  {
+    name: "home",
+    url: "/",
+    icon: "home",
+  },
+  {
+    name: "diet",
+    url: "/diet",
+    icon: "calendar",
+  },
+  {
+    name: "shortcut",
+    url: "/shortcut",
+    icon: "bookmark",
+  },
+  {
+    name: "timetable",
+    url: "/timetable",
+    icon: "timetable",
+  },
+  {
+    name: "about",
+    url: "/about",
+    icon: "menu",
+  },
+];
+
+export default linkInfo;
