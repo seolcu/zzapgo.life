bf3fcc6d75a81a3330f47cb192988ea3eb8ccd24
seolcu <seolcu0112@proton.me>
2021-10-01 12:00:00 +0900

    라우트에 Main, 그 안에 Pages. Main에 전체 swiper 추가

---
+++ src/routes/Main.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+  import { Swiper, SwiperSlide } from "swiper/svelte";
+  import "swiper/css";
+  import Home from "../pages/Home.svelte";
+  import Calendar from "../pages/Calendar.svelte";
+</script>
+
+<Swiper
+  style="height: 100%;"
+  initialSlide={0}
+  slidesPerView={1}
+  spaceBetween={300}
+  centeredSlides={true}
+  on:slideChange={() => console.log("slide change")}
+  on:swiper={(e) => console.log(e.detail[0])}
+>
+  <SwiperSlide><Home /></SwiperSlide>
+  <SwiperSlide><Calendar /></SwiperSlide>
+</Swiper>
