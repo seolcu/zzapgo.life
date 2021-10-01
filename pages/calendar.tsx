f9f718cb4fcd96792df5e339451595f58fdff97d
seolcu <seolcu0112@proton.me>
2021-10-01 12:00:00 +0900

    날짜별 급식 스와이퍼 완성

--- pages/calendar.tsx
+++ pages/calendar.tsx
@@ -3,17 +3,25 @@ import { useState } from "react";
 import FooterComponent from "../components/FooterComponent";
 import HeaderComponent from "../components/HeaderComponent";
 import styles from "../styles/Calendar.module.scss";
+import MealSwiper from "../components/MealSwiper";
 import Calendar from "react-calendar";
 
 const CalendarPage = () => {
   const [dateInfo, setDateInfo] = useState(new Date());
+  const fetcher = (url: string) => fetch(url).then((res) => res.json());
+  const API = `https://open.neis.go.kr/hub/mealServiceDietInfo?KEY=015f0705bbe0482589da35f787d46817&Type=json&pIndex=1&pSize=3&ATPT_OFCDC_SC_CODE=Q10&SD_SCHUL_CODE=8490078&MLSV_YMD=${dateInfo.getFullYear()}${(
+    "0" +
+    (dateInfo.getMonth() + 1)
+  ).slice(-2)}${("0" + dateInfo.getDate()).slice(-2)}`;
   return (
     <>
       <Head>
         <title>캘린더</title>
         <meta name="description" content="캘린더" />
       </Head>
-      <HeaderComponent title="캘린더" />
+      <HeaderComponent
+        title={`${dateInfo.getMonth() + 1}월 ${dateInfo.getDate()}일`}
+      />
       <Calendar
         onChange={setDateInfo}
         value={dateInfo}
@@ -25,6 +33,7 @@ const CalendarPage = () => {
         formatDay={(locale, date) => date.getDate().toString()}
         // 캘린더 css는 react-calendar.scss에서 하기
       />
+      <MealSwiper API={API} fetcher={fetcher} />
       <FooterComponent currentPage={1} />
     </>
   );
