e467dae276de94f6023843831a0cf12c3037817a
seolcu <seolcu0112@proton.me>
2021-10-01 12:00:00 +0900

    themed calendar

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,56 @@
 import { Button } from "@chakra-ui/button";
 import { useColorMode } from "@chakra-ui/color-mode";
-import { Box, Center, Container, Flex, Spacer, Text } from "@chakra-ui/layout";
+import {
+  Box,
+  Center,
+  Container,
+  Flex,
+  Heading,
+  HStack,
+  Spacer,
+  Text,
+} from "@chakra-ui/layout";
 import { Switch } from "@chakra-ui/switch";
 import { useState } from "react";
 import "./App.css";
+import Calendar from "react-calendar";
+import "./Calendar.css";
 
 function App() {
   const { colorMode, toggleColorMode } = useColorMode();
+  const [selectedDate, setSelectedDate] = useState(new Date());
   return (
     <div>
-      <Container>
-        <Flex>
+      <Container centerContent>
+        <Heading>
+          {selectedDate.getFullYear()}년 {selectedDate.getMonth() + 1}월{" "}
+          {selectedDate.getDate()}일
+        </Heading>
+        <Calendar
+          onChange={setSelectedDate}
+          value={selectedDate}
+          calendarType="US"
+          locale="ko-KR"
+          formatDay={(locale: string, date: Date) => date.getDate().toString()}
+          next2Label={null}
+          prev2Label={null}
+          minDetail="month"
+        />
+        <Button
+          colorScheme="yellow"
+          onClick={() => setSelectedDate(new Date())}
+          size="sm"
+        >
+          오늘
+        </Button>
+        <HStack>
           <Text>다크 모드</Text>
-          <Spacer />
           <Switch
             isChecked={colorMode === "dark" ? true : false}
             onChange={toggleColorMode}
-            colorScheme="teal"
+            colorScheme="yellow"
           />
-        </Flex>
+        </HStack>
       </Container>
     </div>
   );
