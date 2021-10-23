5bbc9bb8030a0df4011786f31360b1d7cda8c197
seolcu <gyuwon112@gmail.com>
2021-10-24 00:29:27 +0900

    로고 변경

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,12 @@
 import "../styles/globals.scss";
+import { ThemeProvider } from "next-themes";
 
 const MyApp = ({ Component, pageProps }) => {
-  return <Component {...pageProps} />;
+  return (
+    <ThemeProvider>
+      <Component {...pageProps} />
+    </ThemeProvider>
+  );
 };
 
 export default MyApp;
