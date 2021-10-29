97be38d86a70e97a091163cf55faba5a12ed0cbc
seolcu <gyuwon112@gmail.com>
2021-10-30 01:54:46 +0900

    FooterComponent 가리지 않게 여백용 div 추가

--- components/FooterComponent.js
+++ components/FooterComponent.js
@@ -7,39 +7,44 @@ import styles from "./FooterComponent.module.scss";
 const FooterComponent = ({ currentPage }) => {
   const size = "35";
   return (
-    <footer className={styles.wrapper}>
-      <Link href="/">
-        <a className={styles.a}>
-          <HomeIcon size={size} selected={currentPage == 0 ? true : false} />
-          <div
-            style={
-              currentPage == 0
-                ? { color: "var(--footerIconEnabled)" }
-                : { color: "var(--footerIconDisabled)" }
-            }
-          >
-            홈
-          </div>
-        </a>
-      </Link>
-      <Link href="/calendar">
-        <a className={styles.a}>
-          <CalendarIcon
-            size={size}
-            selected={currentPage == 1 ? true : false}
-          />
-          <div
-            style={
-              currentPage == 1
-                ? { color: "var(--footerIconEnabled)" }
-                : { color: "var(--footerIconDisabled)" }
-            }
-          >
-            캘린더
-          </div>
-        </a>
-      </Link>
-    </footer>
+    <>
+      <footer className={styles.wrapper}>
+        <Link href="/">
+          <a className={styles.a}>
+            <HomeIcon size={size} selected={currentPage == 0 ? true : false} />
+            <div
+              style={
+                currentPage == 0
+                  ? { color: "var(--footerIconEnabled)" }
+                  : { color: "var(--footerIconDisabled)" }
+              }
+            >
+              홈
+            </div>
+          </a>
+        </Link>
+        <Link href="/calendar">
+          <a className={styles.a}>
+            <CalendarIcon
+              size={size}
+              selected={currentPage == 1 ? true : false}
+            />
+            <div
+              style={
+                currentPage == 1
+                  ? { color: "var(--footerIconEnabled)" }
+                  : { color: "var(--footerIconDisabled)" }
+              }
+            >
+              캘린더
+            </div>
+          </a>
+        </Link>
+      </footer>
+
+      {/* 푸터에 가리지 않기 위한 여백 */}
+      <div style={{ height: "100px" }} />
+    </>
   );
 };
 
