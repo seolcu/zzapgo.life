f8f03e50538565358d0f9e8104a2bf1edd41f5e5
seolcu <gyuwon112@gmail.com>
2022-07-07 21:38:45 +0900

    setup chakra ui, set theme

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,25 @@
+import { Button } from "@chakra-ui/button";
+import { useColorMode } from "@chakra-ui/color-mode";
+import { Box, Center, Container, Flex, Spacer, Text } from "@chakra-ui/layout";
+import { Switch } from "@chakra-ui/switch";
 import { useState } from "react";
 import "./App.css";
 
 function App() {
+  const { colorMode, toggleColorMode } = useColorMode();
   return (
     <div>
-      <main></main>
+      <Container>
+        <Flex>
+          <Text>다크 모드</Text>
+          <Spacer />
+          <Switch
+            isChecked={colorMode === "dark" ? true : false}
+            onChange={toggleColorMode}
+            colorScheme="teal"
+          />
+        </Flex>
+      </Container>
     </div>
   );
 }
