f94e1225cc7f6dc72e3344f4d3c1d0cbfd6f49e8
seolcu <gyuwon112@gmail.com>
2022-07-07 20:00:33 +0900

    wiped out

--- components/CardLayout.tsx
+++
@@ -1,7 +0,0 @@
-import styles from "../styles/components/CardLayout.module.scss";
-
-const CardLayout = ({ children }: any) => {
-  return <div className={styles.wrapper}>{children}</div>;
-};
-
-export default CardLayout;
