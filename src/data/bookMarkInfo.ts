adb5f10ef78f781d2605190cac2d99c795fdb367
seolcu <gyuwon112@gmail.com>
2022-07-09 12:01:32 +0900

    margin & padding adjustment, bookmark done

---
+++ src/data/bookMarkInfo.ts
@@ -0,0 +1,34 @@
+const bookMarkInfo = [
+  {
+    id: 1,
+    title: "광양제철고등학교 홈페이지",
+    url: "https://gwangcheol.hs.jne.kr",
+  },
+  {
+    id: 2,
+    title: "리로스쿨 한빛시스템",
+    url: "https://gwangcheol.riroschool.kr",
+  },
+  {
+    id: 3,
+    title: "한빛커뮤니티",
+    url: "https://cafe.naver.com/gwangcheolclub",
+  },
+  {
+    id: 4,
+    title: "급식 신청",
+    url: "http://gwang.i-zone.kr",
+  },
+  {
+    id: 5,
+    title: "자가진단",
+    url: "https://hcs.eduro.go.kr",
+  },
+  {
+    id: 6,
+    title: "짭고라이프 소스 코드",
+    url: "https://github.com/seolcu/zzapgo.life",
+  },
+];
+
+export default bookMarkInfo;
